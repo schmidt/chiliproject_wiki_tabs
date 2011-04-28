dda07b45f8dab3993b07a0a0b366c67212793dfa
Gregor Schmidt <ruby@schmidtwisser.de>
2011-04-28 15:09:14 +0200

    reformatting

--- lib/redmine_wiki_tabs/patches/wiki_controller_patch.rb
+++ lib/redmine_wiki_tabs/patches/wiki_controller_patch.rb
@@ -5,44 +5,49 @@ module RedmineWikiTabs
         base.class_eval do
           unloadable
 
-          def new
-            @page = WikiPage.new(:wiki => @wiki)
-            @page.content = WikiContent.new(:page => @page)
-
-            @content = @page.content_for_version(nil)
-            @content.text = initial_page_content(@page)
-          end
-
-          def new_child
-            find_existing_page
-            return if performed?
-
-            old_page = @page
-
-            new
-
-            @page.parent_id = old_page.id
-            render :action => 'new'
-          end
-
           verify :method => :post, :only => :create, :render => {:nothing => true, :status => :method_not_allowed}
-          def create
-            new
 
-            @page.title     = params[:page][:title]
-            @page.parent_id = params[:page][:parent_id]
+          include InstanceMethods
+        end
+      end
 
-            @content.attributes = params[:content]
-            @content.author = User.current
+      module InstanceMethods
+        def new
+          @page = WikiPage.new(:wiki => @wiki)
+          @page.content = WikiContent.new(:page => @page)
 
-            if @page.save
-              attachments = Attachment.attach_files(@page, params[:attachments])
-              render_attachment_warning_if_needed(@page)
-              call_hook(:controller_wiki_edit_after_save, :params => params, :page => @page)
-              redirect_to :action => 'show', :project_id => @project, :id => @page.title
-            else
-              render :action => 'new'
-            end
+          @content = @page.content_for_version(nil)
+          @content.text = initial_page_content(@page)
+        end
+
+        def new_child
+          find_existing_page
+          return if performed?
+
+          old_page = @page
+
+          new
+
+          @page.parent_id = old_page.id
+          render :action => 'new'
+        end
+
+        def create
+          new
+
+          @page.title     = params[:page][:title]
+          @page.parent_id = params[:page][:parent_id]
+
+          @content.attributes = params[:content]
+          @content.author = User.current
+
+          if @page.save
+            attachments = Attachment.attach_files(@page, params[:attachments])
+            render_attachment_warning_if_needed(@page)
+            call_hook(:controller_wiki_edit_after_save, :params => params, :page => @page)
+            redirect_to :action => 'show', :project_id => @project, :id => @page.title
+          else
+            render :action => 'new'
           end
         end
       end
