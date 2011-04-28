8a97ddf277306d99962c4038ba7697e3efaca83a
Gregor Schmidt <ruby@schmidtwisser.de>
2011-04-28 10:08:45 +0200

    Avoiding collision with chiliproject's default routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 ActionController::Routing::Routes.draw do |map|
-  map.wiki_new       'projects/:project_id/wiki/new',           :controller => 'wiki', :action => 'new'
-  map.wiki_new_child 'projects/:project_id/wiki/:id/new', :controller => 'wiki', :action => 'new_child'
-  map.wiki_create    'projects/:project_id/wiki',     :controller => 'wiki', :action => 'create', :conditions => { :method => :post }
+  map.wiki_new       'projects/:project_id/wiki/new',     :controller => 'wiki', :action => 'new',    :conditions => { :method => :get }
+  map.wiki_create    'projects/:project_id/wiki/new',     :controller => 'wiki', :action => 'create', :conditions => { :method => :post }
+  map.wiki_new_child 'projects/:project_id/wiki/:id/new', :controller => 'wiki', :action => 'new_child', :conditions => { :method => :get }
 end
